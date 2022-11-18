583d76ab9337574e6a7567692d940b502471898d
Davidtt083 <92117303+Davidtt083@users.noreply.github.com>
2022-11-18 17:49:33 -0600

    f

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$('.tile')
+/*$('.tile')
     // tile mouse actions
     .on('mouseover', function(){
       $(this).children('.photo').css({'transform': 'scale('+ $(this).attr('data-scale') +')'});
@@ -19,6 +19,72 @@ $('.tile')
         // set up a background image for each tile based on data-image attribute
         .children('.photo').css({'background-image': 'url('+ $(this).attr('data-image') +')'});
     })
+*/
+$(document).ready(function() {
+  var $main = $('.content__main'),
+    $mainImg = $main.find('#img_cover'),
+    $bool = false,
+    $win = $(window),
+    $widthImg = $mainImg.width(),
+    $heightImg = $mainImg.height(),
+    positionFunc = function(e) {
+      return x = e.pageX - $main.offset().left, y = e.pageY - $main.offset().top;
+    },
+    zoomIn = function(e) {
+      positionFunc(e);
+      $mainImg.animate({
+        left: -x,
+        top: -y,
+        width: $widthImg * 2.01,
+        height: $heightImg * 2.01
+      }, 200, function() {
+        $main.bind('mousemove', function(e) {
+          positionFunc(e);
+          $mainImg.css({
+            left: -x,
+            top: -y
+          });
+        });
+      }).css({
+        cursor: 'zoom-out',
+      });
+      $bool = true;
+    },
+    zoomOut = function() {
+      $mainImg.animate({
+        left: 0,
+        top: 0,
+        width: $widthImg,
+        height: $heightImg
+      }, 100).css({
+        cursor: ''
+      });
+      $main.unbind('mousemove');
+      $bool = false;
+    }
+
+  $main.bind('mousedown', function(e) {
+    if ($bool !== true) {
+      zoomIn(e);
+      $main.css({
+        height: $heightImg
+      });
+    } else {
+      zoomOut();
+      $main.css({
+        height: $(this).height()
+      });
+    }
+  }).bind('mouseout', function() {
+    zoomOut();
+  });
+
+  $win.scroll(function() {
+    if ($bool) {
+      zoomOut();
+    }
+  });
+});
 
 
 
